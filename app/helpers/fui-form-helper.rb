ef7d8eebd083a7444ebd6b567eb7421733e83d80
Gen Tamura <gen.tamura84@gmail.com>
2016-02-29 09:08:51 +0000

    Add checkbox and radio button

--- app/helpers/fui-form-helper.rb
+++ app/helpers/fui-form-helper.rb
@@ -6,7 +6,43 @@ module FuiFormHelper
     end
   end
 
+  def fui_checkbox(text, id, options = {})
+    content_tag :label, class: "checkbox", for: id do
+      concat tag :input, type: "checkbox", id: id, class: "custom-checkbox", data: { toggle: "checkbox" }, checked: options[:checked], disabled: options[:disabled], name: options[:name]
+      concat icon_tags
+      concat text
+    end
+  end
+
+  # Radio button : collection
+  def fui_radio_buttons(name, attrs = {})
+    raise "Empty Error: The second argument hash is empty." if attrs.empty?
+    attrs.each_with_object "" do |(key, value), tags|
+      tags << fui_radio(name, key, value)
+    end.html_safe
+  end
+
+  # Radio button : single
+  def fui_radio_button(name, id, text, options = {})
+    fui_radio(name, id, text, options)
+  end
+
   private
+    def fui_radio(name, id, text, options = {})
+      content_tag :label, class: "radio", for: id do
+        concat tag :input, type: "radio", id: id, class: "custom-radio", name: name, data: { toggle: "radio" }, checked: options[:checked], disabled: options[:disabled]
+        concat icon_tags
+        concat text
+      end
+    end
+
+    def icon_tags
+      content_tag :span, class: "icons" do
+        concat content_tag :span, '', class: "icon-unchecked"
+        concat content_tag :span, '', class: "icon-checked"
+      end
+    end
+
     def self.define_text_field_tag(type)
       define_method "fui_#{type}_text_field_tag" do |name, value = nil, options = {}|
         default_options = { class: " form-control" }
