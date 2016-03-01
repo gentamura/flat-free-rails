1774bc9eae4664faae55f6e45e17660d0d0d7411
Gen Tamura <gen.tamura84@gmail.com>
2016-03-01 01:35:45 +0000

    Add button options

--- app/helpers/fui-button-helper.rb
+++ app/helpers/fui-button-helper.rb
@@ -8,8 +8,19 @@ module FuiButtonHelper
 
   private
     def self.define_button(name)
-      define_method "fui_#{name}_button" do |value, path|
-        link_to value, path, class: "btn btn-#{name}"
+      define_method "fui_#{name}_button" do |value, path, options = {}|
+        class_value = "btn btn-#{name}"
+        class_value << " btn-hg" if options[:hg]
+        class_value << " btn-embossed" if options[:embossed]
+        class_value << " btn-wide" if options[:wide]
+        if options[:tips]
+          link_to path, class: class_value do
+            concat value
+            concat content_tag :span, options[:tips], class: "btn-tip"
+          end
+        else
+          link_to value, path, class: class_value
+        end
       end
     end
 end
